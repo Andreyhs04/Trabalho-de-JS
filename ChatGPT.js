13d80ca878b3258044c5aa456fc87f994a64177f
Andreyhs04 <andrey.hias29@gmail.com>
2023-08-03 16:50:31 -0300

    Initial commit

---
+++ ChatGPT.js
@@ -0,0 +1,50 @@
+function Funcao(){
+    let a=Number(prompt("Qual e a altura"));
+    let b=Number(prompt("Qual e a diametro"));
+    let c=a*b 
+    alert("A firigura tem a media de" +c)
+    let zap=document.getElementById('a_ret')
+    zap.style.color='black'
+    zap.innerHTML=c
+}
+
+function Funcao2(){
+    let a=Number(prompt("Qual e a altura"));
+    let b=Number(prompt("Qual e a diametro"));
+    let c=a*b 
+    alert("A firigura tem a media de" +c)
+    let zap=document.getElementById('b_ret')
+    zap.style.color='black'
+    zap.innerHTML=c
+}
+
+function Funcao3(){
+    let a=Number(prompt("Qual e a altura"));
+    let b=Number(prompt("Qual e a diametro"));
+    let c=a*b 
+    alert("A firigura tem a media de" +c)
+    let zap=document.getElementById('c_ret')
+    zap.style.color='black'
+    zap.innerHTML=c
+}
+
+function Funcao4(){
+    let a=Number(prompt("Qual e a altura"));
+    let b=Number(prompt("Qual e a diametro"));
+    let c=a*b 
+    alert("A firigura tem a media de" +c)
+    let zap=document.getElementById('d_ret')
+    zap.style.color='black'
+    zap.innerHTML=c
+}
+
+function Funcao5(){
+    let a=Number(prompt("Qual e a altura"));
+    let b=Number(prompt("Qual e a diametro"));
+    let c=a*b 
+    alert("A firigura tem a media de" +c)
+    let zap=document.getElementById('e_ret')
+    zap.style.color='black'
+    zap.innerHTML=c
+}
+
